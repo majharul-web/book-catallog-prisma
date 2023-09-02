51c33b99c9efafb8cc139aff7285ac160eb7d328
majharul <majharuljony163@gmail.com>
2023-09-02 10:58:14 +0600

    hash password and return data without password in singnup

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,27 @@
 import { User } from '@prisma/client';
 import prisma from '../../../shared/prisma';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
 
-const signUp = async (userData: User): Promise<User | null> => {
+const signUp = async (userData: User): Promise<Partial<User> | null> => {
+  const hashedPassword = await bcrypt.hash(
+    userData.password,
+    Number(config.bycrypt_salt_rounds)
+  );
+
+  userData.password = hashedPassword;
+
   const result = await prisma.user.create({
     data: userData,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+    },
   });
 
   return result;
