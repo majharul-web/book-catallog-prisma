7de9ebd9836742e71a9f2210136df58e2a8b97b5
majharul <majharuljony163@gmail.com>
2023-09-03 00:04:44 +0600

    [Done] Book search

--- src/app/modules/book/book.interface.ts
+++ src/app/modules/book/book.interface.ts
@@ -1,7 +1,7 @@
 export type IBookFilterRequest = {
   search?: string | undefined;
   title?: string | undefined;
-  category: string | undefined;
+  category?: string | undefined;
   maxPrice?: number | undefined;
   minPrice?: number | undefined;
 };
